69bc67ea11d0bcb5112d903c9e3ed640e95f7e01
takenokoya <takuya1002@gmail.com>
2019-08-18 19:56:23 +0900

    add admin_users_controller and create simple CRUD

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,26 @@
 class Task < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 30 }
+  validate :validate_name_not_including_comma
+
+  before_validation :set_nameless_name
+
+
+  def validate_name_not_including_comma
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
+
+  private
+
+  def set_nameless_name
+    self.name = '名前無し' if name.blank?
+  end
+
+
+
+
+
+
+
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_24_214647) do
+ActiveRecord::Schema.define(version: 2019_08_18_102037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
   end
 
 end
